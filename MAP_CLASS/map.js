84c841234c967d5520e2bca4d3fd33d51e0c9f65
PHANI <phanipspkmech@gmail.com>
2023-11-08 08:11:13 -0800

    map class

--- MAP_CLASS/map.js
+++ MAP_CLASS/map.js
@@ -16,5 +16,22 @@ let getValue=map.get('name')
 console.log(getValue);
 console.log(map.get('highesteducation'));
 
-//keys() used to get key names by the form of mapiterator
+//keys() used to get keys by the form of mapiterator
 console.log(map.keys());
+//values() used to get values by the form of mapiterator
+console.log(map.values());
+
+//delete() used to delete key value pair , input as key , returns boolean value
+console.log(map.delete('salary'));
+console.log(map);
+
+//has() used to check particular key is present or not
+//and it returns a boolean value 
+console.log(map.has('id'));
+
+//size is a property for map returns size of map like number
+console.log(map.size);
+
+//clear() used the clear complete map object
+map.clear()
+console.log(map);
